6544fb8db41b519f766effd8127fbd0e29ff3848
Ewonou Aziakonou <aewonou@gmail.com>
2012-07-15 14:48:09 -0500

    Second requirement test - passed

--- src/com/twu28/biblioteca/Biblioteca.java
+++ src/com/twu28/biblioteca/Biblioteca.java
@@ -3,15 +3,21 @@ import java.util.*;
 import java.io.*;
 public class Biblioteca {
     private final static String firstMsg = "Welcome";
+    private final static String[] menuOptions = {"Menu1","Menu2","Menu 3"};
 
 public void main(String[] Args){
     System.out.println(firstMsg);
+    System.out.println("Please Select from the following Menu Options");
+    for (String item : menuOptions){
+        System.out.print(item + " ");
+    }
+
 }
     public static String getMessage() {
         return firstMsg;
     }
 
     public static int getNumberOfOptions() {
-        return 0;
+        return menuOptions.length;
     }
 }
