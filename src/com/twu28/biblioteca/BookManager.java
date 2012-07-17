8ad431f48deb6796a42c59aa4b1e10b7f52b9b18
Ewonou Aziakonou <aewonou@gmail.com>
2012-07-17 05:05:29 -0500

    Requirement5 test passed

--- src/com/twu28/biblioteca/BookManager.java
+++ src/com/twu28/biblioteca/BookManager.java
@@ -1,6 +1,5 @@
 package com.twu28.biblioteca;
 
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -10,6 +9,27 @@ public class BookManager {
     public int displayBooks()
     {
         int bookDisplayed = 0;
+        try
+        {
+            File bookAuthor = new File("booksAndAuthors.txt");
+
+            FileReader bookReader = new FileReader(bookAuthor);
+
+            BufferedReader reader = new BufferedReader(bookReader);
+
+            String line = null ;
+
+            while ((line = reader.readLine())!= null)
+            {
+                System.out.println(line);
+                bookDisplayed++;
+            }
+            reader.close();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
 
         return bookDisplayed;
     }
